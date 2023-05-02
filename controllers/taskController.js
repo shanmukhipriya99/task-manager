dacad33e930a65410cab844d2b7562d4ec42dfc6
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-01 23:32:26 -0700

    Task routes

---
+++ controllers/taskController.js
@@ -0,0 +1,20 @@
+// TODO: Get all tasks
+exports.getAllTasks = (req, res) => {
+    res.send('All Tasks');
+};
+// TODO: Create a task
+exports.createTask = (req, res) => {
+    res.send('Created Task');
+};
+// TODO: Get a single task
+exports.getTask = (req, res) => {
+    res.send('Get Single Task');
+};
+// TODO: Update a task
+exports.updateTask = (req, res) => {
+    res.send('Updated Task');
+};
+// TODO: Delete a task
+exports.deleteTask = (req, res) => {
+    res.send('Deleted Task');
+};
