39687503e6254d3360ca89a77d502b16c626bdfd
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-01 23:32:57 -0700

    middleware and task routes

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const taskRouter = require('./routes/taskRoutes');
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Task Manager App');
+});
+app.use('/api/v1/tasks', taskRouter);
 
 const port = process.env.PORT || 3000;
 
