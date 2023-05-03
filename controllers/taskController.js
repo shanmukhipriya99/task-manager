42447019d1674430604453707b58ae0923983f6e
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-02 21:41:27 -0700

    formatted

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,20 +1,20 @@
 // TODO: Get all tasks
 exports.getAllTasks = (req, res) => {
-    res.send('All Tasks');
+  res.send('All Tasks');
 };
 // TODO: Create a task
 exports.createTask = (req, res) => {
-    res.send('Created Task');
+  res.send('Created Task');
 };
 // TODO: Get a single task
 exports.getTask = (req, res) => {
-    res.send('Get Single Task');
+  res.send('Get Single Task');
 };
 // TODO: Update a task
 exports.updateTask = (req, res) => {
-    res.send('Updated Task');
+  res.send('Updated Task');
 };
 // TODO: Delete a task
 exports.deleteTask = (req, res) => {
-    res.send('Deleted Task');
+  res.send('Deleted Task');
 };
