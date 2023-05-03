d6aaa17eae902c07e21e2c3c121223e5205e28a5
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-03 12:47:13 -0700

    try catch refactor

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,72 +1,46 @@
 const Task = require('../models/taskModel');
+const catchAsync = require('../middleware/catchAsync');
 
 // NOTE: Get all tasks
-exports.getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res
-      .status(200)
-      .send({ success: true, quantity: tasks.length, data: tasks });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({ success: false, message: 'There was an error!💥' });
-  }
-};
+exports.getAllTasks = catchAsync(async (req, res) => {
+  const tasks = await Task.find();
+  res.status(200).send({ success: true, quantity: tasks.length, data: tasks });
+});
 // NOTE: Create a task
-exports.createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).send({ success: true, data: task });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({ success: false, message: 'There was an error!💥' });
-  }
-};
+exports.createTask = catchAsync(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).send({ success: true, data: task });
+});
 // NOTE: Get a single task
-exports.getTask = async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id);
-    if (!task) {
-      return res
-        .status(404)
-        .send({ success: false, message: `No task with id: ${req.params.id}` });
-    }
-    res.status(200).send({ success: true, data: task });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({ success: false, message: 'There was an error!💥' });
+exports.getTask = catchAsync(async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  if (!task) {
+    return res
+      .status(404)
+      .send({ success: false, message: `No task with id: ${req.params.id}` });
   }
-};
+  res.status(200).send({ success: true, data: task });
+});
 // NOTE: Update a task
-exports.updateTask = async (req, res) => {
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      runValidators: true,
-      new: true,
-    });
-    if (!task) {
-      return res
-        .status(404)
-        .send({ success: false, message: `No task with id: ${req.params.id}` });
-    }
-    res.status(200).send({ success: true, data: task });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({ success: false, message: 'There was an error!💥' });
+exports.updateTask = catchAsync(async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  if (!task) {
+    return res
+      .status(404)
+      .send({ success: false, message: `No task with id: ${req.params.id}` });
   }
-};
+  res.status(200).send({ success: true, data: task });
+});
 // NOTE: Delete a task
-exports.deleteTask = async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if (!task) {
-      return res
-        .status(404)
-        .send({ success: false, message: `No task with id: ${req.params.id}` });
-    }
-    res.status(200).send({ success: true });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({ success: false, message: 'There was an error!💥' });
+exports.deleteTask = catchAsync(async (req, res) => {
+  const task = await Task.findByIdAndDelete(req.params.id);
+  if (!task) {
+    return res
+      .status(404)
+      .send({ success: false, message: `No task with id: ${req.params.id}` });
   }
-};
+  res.status(200).send({ success: true });
+});
