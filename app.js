f2a109251ad0cdbb46fe891a807863fbc414cca0
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-03 15:06:51 -0700

    global error handling middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const taskRouter = require('./routes/taskRoutes');
 const AppError = require('./middleware/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const { globalErrorHandler } = require('./middleware/globalErrorHandler');
 
 // Middleware
 app.use(express.static('./public'));
