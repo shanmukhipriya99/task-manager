69189e60b3a92044654de4f3aef15465b22683e6
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-02 23:35:22 -0700

    error handling with try catch

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,18 +2,33 @@ const Task = require('../models/taskModel');
 
 // NOTE: Get all tasks
 exports.getAllTasks = async (req, res) => {
-  const tasks = await Task.find();
-  res.status(200).send({ success: true, data: tasks });
+  try {
+    const tasks = await Task.find();
+    res.status(200).send({ success: true, data: tasks });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ success: false, message: 'There was an error!💥' });
+  }
 };
 // NOTE: Create a task
 exports.createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).send({ success: true, data: task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).send({ success: true, data: task });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ success: false, message: 'There was an error!💥' });
+  }
 };
 // NOTE: Get a single task
 exports.getTask = async (req, res) => {
-  const task = await Task.findById(req.params.id);
-  res.status(200).send({ success: true, data: task });
+  try {
+    const task = await Task.findById(req.params.id);
+    res.status(200).send({ success: true, data: task });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ success: false, message: 'There was an error!💥' });
+  }
 };
 // TODO: Update a task
 exports.updateTask = (req, res) => {
