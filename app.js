c06f0ac2b90d3fecb6fd229b2c3b1150057885ee
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-03 14:54:48 -0700

    global error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const taskRouter = require('./routes/taskRoutes');
+const AppError = require('./middleware/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 // Middleware
 app.use(express.static('./public'));
@@ -9,8 +11,11 @@ app.use(express.json());
 // Routes
 app.use('/api/v1/tasks', taskRouter);
 app.all('*', (req, res, next) => {
-  res.status(404).send(`Route doesn't exist!`);
-  next();
+  // res.status(404).send(`Route doesn't exist!`);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+// global error handling middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
