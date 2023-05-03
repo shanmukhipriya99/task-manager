a1db780cdb4c2289e7d13a23aa8c7ca6578ceb15
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-03 12:34:02 -0700

    invalid routes

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ app.use(express.json());
 
 // Routes
 app.use('/api/v1/tasks', taskRouter);
+app.all('*', (req, res, next) => {
+  res.status(404).send(`Route doesn't exist!`);
+  next();
+});
 
 module.exports = app;
