b8860145501a7dc3cf2e035f20d7c16281f7db8a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-02 23:28:03 -0700

    basic validation

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, 'Must have a name'],
+    trim: true,
+    maxLength: [50, 'Must be below 50 characters'],
+  },
+  completed: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Task', taskSchema);
